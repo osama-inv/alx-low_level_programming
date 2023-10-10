0bc018b9d7e014f2e3ae524aab12a5a0bf86f876
osama <osamamahmoudac@gmail.com>
2023-10-10 12:20:45 -0700

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - def Important.
+ * @c : a char
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+if (c <= 'z' && c >= 'a' || c <= 'z' && c >= 'a')
+return (1);
+return (0);
+}
+
