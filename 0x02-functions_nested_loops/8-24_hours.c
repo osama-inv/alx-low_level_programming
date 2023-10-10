ff91fce5aaabbea3dfe7a042f5ce3b6d82d40dcf
osama <osamamahmoudac@gmail.com>
2023-10-10 13:34:49 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,34 +7,34 @@
 
 void jack_bauer(void)
 {
-char i1 = '0';
-char i2 = '0';
-char i3 = '0';
-char i4 = '0';
+char a1 = '0';
+char b2 = '0';
+char c3 = '0';
+char d4 = '0';
 int i = 1440;
 
 while (i--)
 {
-_putchar(i1);
-_putchar(i2);
+_putchar(a1);
+_putchar(b2);
 _putchar(':');
-_putchar(i3);
-_putchar(i4++);
+_putchar(c3);
+_putchar(d4++);
 _putchar('\n');
-if (i4 == '9' + 1)
+if (d4 == '9' + 1)
 {
-i3++;
-i4 = '0';
+c3++;
+d4 = '0';
 }
-if (i3 == '6')
+if (c3 == '6')
 {
-i2++;
-i3 = '0';
+b2++;
+c3 = '0';
 }
-if (i2 == '9' + 1)
+if (b2 == '9' + 1)
 {
-i1++;
-i2 = '0';
+a1++;
+b2 = '0';
 }
 
 }
