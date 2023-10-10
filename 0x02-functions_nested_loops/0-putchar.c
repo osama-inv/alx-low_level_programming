8539bd45b10b9df42a55a89ef06bce3d76852651
osama <osamamahmoudac@gmail.com>
2023-10-10 07:00:51 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+int i = 0;
+while(i<=7){
+_putchar(word[i]);
+i++;
+}
+_putchar('\n')
+return (0);
+}
+
