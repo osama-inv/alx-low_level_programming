6dbb6eb768c58284f01cc598ebe5d47bbbcfd2c3
osama <osamamahmoudac@gmail.com>
2023-10-12 09:11:23 -0700

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,10 +8,11 @@
 
 void print_square(int size)
 {
+int i, j;
 
-for (int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
-for (int j = 0; j < size; j++)
+for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
