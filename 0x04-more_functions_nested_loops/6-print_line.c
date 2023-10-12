34e723dcb1ffae3d89ca44a903aac76208bce0f9
osama <osamamahmoudac@gmail.com>
2023-10-12 07:10:13 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - a def
+ * @n:char
+ * Return: Always 0 or 1
+ */
+
+void print_line(int n)
+{
+if (!n < 1)
+while (n--)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
