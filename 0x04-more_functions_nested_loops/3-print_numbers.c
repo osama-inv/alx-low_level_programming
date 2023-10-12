4f9ab08afd15e1042a5a7fc400b534255cf8a85a
osama <osamamahmoudac@gmail.com>
2023-10-12 03:57:27 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - a def
+ * Return: Always 0 or 1
+ */
+
+void print_numbers(void)
+{
+char him = '0';
+while (him <= '9')
+{
+_putchar(him);
+him++;
+}
+_putchar('\n');
+}
