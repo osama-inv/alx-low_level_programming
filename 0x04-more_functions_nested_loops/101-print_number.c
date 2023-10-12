cefe9b26823c87ea6f0c7e09d0260bc3c68724f5
osama <osamamahmoudac@gmail.com>
2023-10-12 16:03:38 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,43 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_number - a def
- * @n:int
- * Return: Always 0 or 1
+ * print_number - print any integer using putchar
+ * @n: integer to be printed
  */
 
 void print_number(int n)
 {
-int new_n = n;
-if (new_n < 0)
-{
-putchar('-');
-new_n *= -1;
-}
-if (new_n == 0)
-putchar('0');
-else
-{
-int rev = 0;
+	int count, x, y, k;
 
-while (new_n)
-{
-rev *= 10;
-rev += new_n % 10;
-new_n /= 10;
-}
-while (rev)
-{
-putchar('0' + rev % 10);
-rev /= 10;
-}
-}
+	x = 0;
+	y = 1000000000;
+
+	if (n == 0)
+		_putchar('0');
+	else if (n > 0)
+		n *= -1;
+	else
+	       	_putchar('-');
+	for (count = 0; count < 10; count++)
+	{
+		if (n / y == 0 && x == 0)
+		{
+			y /= 10;
+			
+			continue;
+		}
+		else if (x == 0)
+		{
+			_putchar(-(n / y) + '0');
+			x += 1;
+		}
+		else
+		{
+			k = (-(n / y) % 10);
+			if (k < 0)
+				k *= -1;
+			_putchar(k + '0');
+		}
+		y /= 10;
+	}
 }
