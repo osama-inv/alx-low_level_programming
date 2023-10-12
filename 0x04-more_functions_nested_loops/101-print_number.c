84f83b0c93fb86ce1c374ca787e0d6e7ad354e76
osama <osamamahmoudac@gmail.com>
2023-10-12 16:29:58 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,46 @@
 #include "main.h"
 /**
- * print_number - print an int numbers.
- * @n: number tested
- * Return: Always 0.
+ * print_number - to turn to char
+ * @n: int
+ * Return: Always 0 or else.
  */
 void print_number(int n)
 {
-	int i, j, digit, digits, power;
-	unsigned int temp, numchar, number;
+	int i, j, digit, digits, oss;
+	unsigned int hold, numm, numbr;
 
 	digit = 0;
 	if (n < 0)
 	{
 		_putchar('-');
-		temp = -n;
+		hold = -n;
 	}
 	else
 	{
-		temp = n;
+		hold = n;
 	}
 
-	number = temp;
+	numbr = hold;
 
-	while (number >= 10)
+	while (numbr >= 10)
 	{
-		number = number / 10;
+		numbr = numbr / 10;
 		digit++;
 	}
 	digits = digit + 1;
-	power = 1;
+	oss = 1;
 	i = 1;
 
 	while (i < digits)
 	{
-		power = power * 10;
+		oss = oss * 10;
 		i++;
 	}
-	j = power;
+	j = oss;
 	while (j >= 1)
 	{
-		numchar = (temp / j) % 10;
-		_putchar(numchar + '0');
+		numm = (hold / j) % 10;
+		_putchar(numm + '0');
 		j = j / 10;
 	}
 }
