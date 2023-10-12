56cde895a5e4903b64c704b9d430adbf66dadcbf
osama <osamamahmoudac@gmail.com>
2023-10-12 11:17:07 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,18 +9,35 @@
 int main(void)
 {
 int i;
+
 for (i = 1; i <= 100; i++)
 {
-if (i % 15 == 0)
-printf("%s ", "FizzBuzz");
-else if (i % 3 == 0)
-printf("%s ", "Fizz");
-else if (i % 5 == 0)
-printf("%s ", "Buzz");
-else
-printf("%d ", i);
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
 }
-printf("%c", '\n');
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+
+if (i < 100)
+        {
+printf(" ");
+}
+else
+{
+printf("\n");
+}
+}
 
 return (0);
 }
