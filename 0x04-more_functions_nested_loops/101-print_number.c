7b635417ebf1e37a984509c781db92db63aa70e8
osama <osamamahmoudac@gmail.com>
2023-10-12 15:50:47 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_number - a def
+ * @n:int
+ * Return: Always 0 or 1
+ */
+
+void print_number(int n)
+{
+int smaller = 0;
+
+if (n < 0)
+{
+smaller = 1;
+n *= -1;
+}
+
+if (n == 0)
+_putchar('0');
+else
+{
+int rev = 0;
+
+while (n)
+{
+rev *= 10;
+rev += n % 10;
+n /= 10;
+}
+if (smaller)
+_putchar('-');
+while (rev)
+{
+_putchar('0' + rev % 10);
+rev /= 10;
+}
+}
+}
