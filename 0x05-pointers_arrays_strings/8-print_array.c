32bc728265f71b8f37c4bc62d8fe596ea75e7c25
osama <osamamahmoudac@gmail.com>
2023-10-16 14:14:22 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - a def that does something.
+ * @a: char.
+ * @n: char.
+ * Return: Always 0 (Success)
+ */
+
+void print_array(int *a, int n)
+{
+int rate = 0;
+while (n--)
+{
+if (n > 0)
+printf("%d, ",a[rate++]);
+else
+printf("%d\n",a[rate++]);
+}
+}
