c65bbcb47885c6f5bbebf927ac4700b85c677720
osama <osamamahmoudac@gmail.com>
2023-10-16 12:46:38 -0700

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - a def that does something.
+ * @s: char.
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+
+return length;
+}
+
+/**
  * print_rev - a def that does something.
  * @s: char.
  * Return: Always 0 (Success)
