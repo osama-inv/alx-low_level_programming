97dd8e089f06ff91881d565e0f8a4219ced7bb2f
osama <osamamahmoudac@gmail.com>
2023-10-16 13:10:38 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - a def that does something.
+ * @s: char.
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return length;
+}
+
+/**
+ * rev_string - a def that does something.
+ * @s: char.
+ * Return: Always 0 (Success)
+ */
+
+void rev_string(char *s)
+{
+int last = _strlen(s)-1;
+int first = 0;
+
+while (last != first)
+{
+char temp = *(s + last);
+*(s + last) = *(s + first);
+*(s + first) = temp;
+first++;
+last--;
+}
+}
