c46561cb36d0ce870edf981a8ee00205cad2d392
osama <osamamahmoudac@gmail.com>
2023-10-16 12:27:07 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - a def that does something.
+ * @n: char.
+ * Return: Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
