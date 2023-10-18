cf3ab433a39fb7f7d053528ce44e42da2af6e29c
osama <osamamahmoudac@gmail.com>
2023-10-18 07:17:07 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - a def that does something.
+ * @a: char.
+ * @n: char.
+ * Return: Always 0 (Success)
+ */
+
+void reverse_array(int *a, int n)
+{
+int *last = (a + n) -1;
+int temp;
+int num = n / 2;
+
+while (num--)
+{
+temp = *a;
+*a++ = *last;
+*last-- = temp;
+}
+}
