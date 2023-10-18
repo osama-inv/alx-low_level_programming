3a29d4b7b8cae2f33ec6b813ddae22bf50685c81
osama <osamamahmoudac@gmail.com>
2023-10-18 04:30:07 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - a def that does something.
+ * @dest: char.
+ * @src: char.
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int *dest_back = dest;
+int open = 0;
+
+for (int i = 0; i < 98; i++)
+{
+if (!*(dest + i) || open)
+{
+*(dest + i) = *src;
+src++;
+open = 1;
+if (!*src)
+break;
+}
+}
+return dest_back;
+}
