f4cf902bb21ec924032b4b5332eff4005e2a0ae7
osama <osamamahmoudac@gmail.com>
2023-10-19 16:31:31 -0700

    commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,10 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-if (src[i])
-dest[i] = src[i];
-else
-dest[i] = '\0';
+*(dest + i) = *src;
+if (!*src)
+src--;
+src++;
 }
 return (dest);
 }
