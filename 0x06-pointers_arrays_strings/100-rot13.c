1317e12f5488faeb068510ecd494535b60c61363
osama <osamamahmoudac@gmail.com>
2023-10-19 04:52:38 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - a def that does something.
+ * @str: char.
+ * Return: Always 0 or 1
+ */
+
+char *rot13(char *str)
+{
+if (str == NULL)
+return NULL;
+int i;
+for (i = 0; str[i]; i++)
+{
+char base = 'a';
+char c = str[i];
+
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z' && (base -= 32)))
+str[i] = (c - base + 13) % 26 + base;
+}
+
+return (str);
+}
