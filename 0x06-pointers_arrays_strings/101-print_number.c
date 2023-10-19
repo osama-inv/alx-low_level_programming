bcf39fb596fc2883b6404bc647e072bdf2770267
osama <osamamahmoudac@gmail.com>
2023-10-19 05:47:53 -0700

    commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,15 +1,14 @@
 #include "main.h"
-
 /**
- * print_number - a def that does something.
+ * print_digit - a def that does something.
  * @n: char.
  * Return: Always 0 or 1
  */
-
 void print_number(int n)
 {
 	unsigned int num;
 	num = n;
+    
 	if (n < 0)
 	{
 		_putchar(45);
