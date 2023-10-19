64637d234d472e8b1a4f9822891ba7c559010f33
osama <osamamahmoudac@gmail.com>
2023-10-19 05:51:21 -0700

    commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,17 +6,17 @@
  */
 void print_number(int n)
 {
-	unsigned int num;
-	num = n;
-    
-	if (n < 0)
-	{
-		_putchar(45);
-		num = -n;
-	}
-	if (num / 10)
-	{
-		print_number(num / 10);
-	}
-	_putchar((num % 10) + '0');
+unsigned int num;
+num = n;
+
+if (n < 0)
+{
+_putchar(45);
+num = -n;
+}
+if (num / 10)
+{
+print_number(num / 10);
+}
+_putchar((num % 10) + '0');
 }
