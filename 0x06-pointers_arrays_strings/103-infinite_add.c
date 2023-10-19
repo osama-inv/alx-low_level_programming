0fd83bfab63c1eebe0f859a80f94947692df193e
osama <osamamahmoudac@gmail.com>
2023-10-19 07:08:17 -0700

    commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,43 +1,45 @@
 #include "main.h"
 /**
- * infinite_add - a def that does something.
- * @n1: char.
- * @n2: char.
- * @r: char.
- * @size_r: char.
- * Return: Always 0 or 1
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ *
+ * Return: address of r or 0
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i = 0, j = 0, k = 0, l1 = 0, l2 = 0, sum = 0;
-char tmp;
-
-while (n1[l1 + 1])
-l1++;
-while (n2[l2 + 1])
-l2++;
-if (l1 >= size_r || l2 >= size_r)
+int i, j, k, l, m, n;
+for (i = 0; n1[i]; i++)
+;
+for (j = 0; n2[j]; j++)
+;
+if (i > size_r || j > size_r)
 return (0);
-for (i = l1, j = l2; i >= 0 || j >= 0; i--, j--, k++)
+m = 0;
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 {
-sum += (i >= 0 ? n1[i] - '0' : 0);
-sum += (j >= 0 ? n2[j] - '0' : 0);
-r[k] = sum % 10 + '0';
-sum /= 10;
+n = m;
+if (i >= 0)
+n += n1[i] - '0';
+if (j >= 0)
+n += n2[j] - '0';
+if (i < 0 && j < 0 && n == 0)
+{
+break;
 }
-if (sum && k < size_r - 1)
-r[k++] = sum + '0';
-
-if (k == size_r)
-return (0);
+m = n / 10;
+r[k] = n % 10 + '0';
+}
 r[k] = '\0';
-for (i = 0; i < k / 2; i++)
+if (i >= 0 || j >= 0 || m)
+return (0);
+for (k -= 1, l = 0; l < k; k--, l++)
 {
-tmp = r[i];
-r[i] = r[k - i - 1];
-r[k - i - 1] = tmp;
+m = r[k];
+r[k] = r[l];
+r[l] = m;
 }
 return (r);
 }
-
