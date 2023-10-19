ebb4a2fb3fe1484da65f9febe10c42829d4c26f6
osama <osamamahmoudac@gmail.com>
2023-10-19 02:09:09 -0700

    commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * saperoator - a def that does something.
+ * @c: char.
+ * Return: Always 0 or 1
+ */
+
+int saperoator(char c);
+{
+char *seperators = ",;.!?\"(){} \n\t";
+
+while (*seperators)
+{
+if (*seperators++ == c)
+return (1);
+}
+return (0);
+}
+
+/**
  * cap_string - a def that does something.
  * @str: char.
  * Return: Always 0 or 1
@@ -11,11 +29,7 @@ char *cap_string(char *str)
 int i = 0;
 while (str[i])
 {
-if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
-str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
-str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
-str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
-str[i - 1] == '{' || str[i - 1] == '}')
+if (saperoator(str[i]))
 {
 if (str[i] >= 'a' && str[i] <= 'z')
 {
