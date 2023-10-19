e8a7b957f64dc47680751db936b68ac0903c69af
osama <osamamahmoudac@gmail.com>
2023-10-19 05:26:34 -0700

    commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 int divisor = 1;
 int digit;
 if (n < 0) {
-putchar('-');
+_putchar('-');
 n = -n;
 }
 while (n / divisor >= 10) { d = 100
@@ -19,7 +19,7 @@ divisor *= 10;
 }
 while (divisor != 0) {
 digit = n / divisor;
-putchar(digit + '0');
+_putchar(digit + '0');
 n %= divisor; // 22
 divisor /= 10; // 10
 }
