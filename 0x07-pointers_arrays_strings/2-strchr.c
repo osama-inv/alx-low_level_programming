7666dee1795396b7b63c0881559127b59250d60b
osama <osamamahmoudac@gmail.com>
2023-10-23 03:43:48 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - a def that does something.
+ * @s: char.
+ * @c: char.
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s == c)
+return (s);
+s++;
+}
+return (NULL);
+}
