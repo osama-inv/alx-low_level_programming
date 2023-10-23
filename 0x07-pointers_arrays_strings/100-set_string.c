a33e1846709f952a4fba75b6d85fa1662a63c23a
osama <osamamahmoudac@gmail.com>
2023-10-23 06:02:41 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - a def that does something.
+ * @s: char.
+ * @to: char.
+ * Return: Always 0 (Success)
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
