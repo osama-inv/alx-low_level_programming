1b262dd77b8a73740fb0e1520c4e3cf3bc14b2a3
osama <osamamahmoudac@gmail.com>
2023-10-23 02:45:09 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - a def that does something.
+ * @s: char.
+ * @b: char.
+ * @n: char.
+ * Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+    
+while (n--)
+s[i++] = b;
+        
+return (s);
+}
