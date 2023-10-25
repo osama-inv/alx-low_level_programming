695c7685d147cf7ebc380334bffe0603e32559fc
osama <osamamahmoudac@gmail.com>
2023-10-25 08:24:33 -0700

    commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,33 +1,41 @@
 #include "main.h"
 
 /**
+ * getlast - a def that does something.
+ * @c: char.
+ * Return: Always 0 (Success)
+ */
+char *getlast(char *c)
+{
+    if (*c && *(c + 1))
+        getlast(c + 1);
+    else
+        return (c);
+}
+/**
+ * getans - a def that does something.
+ * @l: char.
+ * @f: char.
+ * Return: Always 0 (Success)
+ */
+int getans(char *f, char *l)
+{
+    if (*f == *l)
+    {
+        if (f == l || (f + 1) == l)
+            return (1);
+        getans(f + 1, l - 1);
+    }
+    else
+        return (0);
+}
+/**
  * is_palindrome - a def that does something.
  * @s: char.
  * Return: Always 0 (Success)
  */
 int is_palindrome(char *s)
 {
-char *last = s;
-while(*(last + 1))
-{
-last += 2;
-if (!*last)
-{
-last--;
-break;
-}
-}
-while (s != last)
-{
-if (*s == *last)
-{
-s++;
-last--;
-if ((s - 1) == last)
-break;
-}
-else
-return (0);
-}
-return (1);
+    char *last = getlast(s);
+    return getans(s, last);
 }
