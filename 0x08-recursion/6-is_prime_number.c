8b78fa3162661f7ceda36b6d74dc418923497b96
osama <osamamahmoudac@gmail.com>
2023-10-25 07:37:24 -0700

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * is_prime_number - a def that does something.
- * @n: char.
- * Return: Always 0 (Success)
- */
-
-int is_prime_number(int n)
-{
-if (n <= 1)
-return (0);
-return (helper(n, n/2));
-}
-/**
  * helper - a def that does something.
  * @n: char.
  * @div: char.
@@ -26,3 +14,17 @@ if (n % div == 0)
 return (0);
 return (helper(n, div - 1));
 }
+
+/**
+ * is_prime_number - a def that does something.
+ * @n: char.
+ * Return: Always 0 (Success)
+ */
+
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+return (helper(n, n/2));
+}
+
