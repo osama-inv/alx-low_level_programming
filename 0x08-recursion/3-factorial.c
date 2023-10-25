65651d0589b586b32dd420d03b1374ffaf49e5b6
osama <osamamahmoudac@gmail.com>
2023-10-25 06:05:02 -0700

    commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,7 +2,7 @@
 
 /**
  * factorial - a def that does something.
- * @c: char.
+ * @n: char.
  * Return: Always 0 (Success)
  */
 
@@ -12,5 +12,5 @@ if (n < 0)
 return (-1);
 if (n == 1 || n == 0)
 return (1);
-return (n * factorial(n-1));
+return (n * factorial(n - 1));
 }
