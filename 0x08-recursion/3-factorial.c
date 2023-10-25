e0d99cc79d7651c1d1df65e43701707aa5b61dde
osama <osamamahmoudac@gmail.com>
2023-10-25 06:04:13 -0700

    commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - a def that does something.
+ * @c: char.
+ * Return: Always 0 (Success)
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 1 || n == 0)
+return (1);
+return (n * factorial(n-1));
+}
