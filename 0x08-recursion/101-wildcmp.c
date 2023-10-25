2f91c9bf1b08b866a75e9b6429a887240242edf5
osama <osamamahmoudac@gmail.com>
2023-10-25 09:48:44 -0700

    commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,34 @@
 #include "main.h"
-/**
-* substring_match - check if a substring after wildcard matches s1
-* @s1: one string
-* @s2: one string
-* @after_wldcd: placeholder for position right after wildcard
-* Return: 1 if matched, 0 if not
-*/
 
-int substring_match(char *s1, char *s2, char *after_wldcd)
+/**
+ * seethat - a def that does something.
+ * @s1: char.
+ * @s2: char.
+ * @charrr: char.
+ * Return: Always 0 (Success)
+ */
+
+int seethat(char *s1, char *s2, char *charrr)
 {
 if (*s1 == '\0' && *s2 == '\0')
 return (1);
 if (*s1 == '\0' && *s2 == '*')
-return (substring_match(s1, s2 + 1, s2 + 1));
+return (seethat(s1, s2 + 1, s2 + 1));
 if (*s1 == '\0' && *s2 != '\0')
 return (0);
 if (*s2 == '*')
-return (substring_match(s1, s2 + 1, s2 + 1));
+return (seethat(s1, s2 + 1, s2 + 1));
 if (*s1 == *s2)
-return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+return (seethat(s1 + 1, s2 + 1, charrr));
 else
-return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+return (seethat(s1 + 1, charrr, charrr));
 }
 
 /**
-* wildcmp - compare if string with wildcard mattches
-* @s1: one string
-* @s2: one string
-* Return: 1 if matched, 0 if not
+* wildcmp - a def that does something.
+* @s1: char.
+* @s2: char.
+* Return: Always 0 (Success)
 */
 
 int wildcmp(char *s1, char *s2)
@@ -37,7 +38,7 @@ return (1);
 if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
 else if (*s2 == '*')
-return (substring_match(s1, (s2 + 1), (s2 + 1)));
+return (seethat(s1, (s2 + 1), (s2 + 1)));
 else
 return (0);
 }
