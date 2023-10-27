774ad35c1b2e4897fa731236fa9361c37e28212c
osama <osamamahmoudac@gmail.com>
2023-10-27 11:37:10 -0700

    commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * @argc: - char
+ * @argv: - char
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
