9c2bcb0053aefa20936cb211c428178a821c9e6d
osama <osamamahmoudac@gmail.com>
2023-10-27 12:14:38 -0700

    commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,19 +8,24 @@
  * Return: 1 if positive integer, 0 otherwise.
  */
 
-int main(int argc, char *argv[]) {
-if (argc != 2) {
+int main(int argc, char *argv[])
+{
+int cents = atoi(argv[1]);
+int coins[] = {25, 10, 5, 2, 1};
+int num_coins = 0;
+int i;
+if (argc != 2)
+{
 printf("Error\n");
 return (1);
 }
-int cents = atoi(argv[1]);
-if (cents < 0) {
+if (cents < 0)
+{
 printf("0\n");
 return (0);
 }
-int coins[] = {25, 10, 5, 2, 1};
-int num_coins = 0;
-for (int i = 0; i < 5; i++) {
+for (i = 0; i < 5; i++)
+{
 num_coins += cents / coins[i];
 cents %= coins[i];
 }
