ea86838d0bca9611672fcdaecdd44154eb49206e
osama <osamamahmoudac@gmail.com>
2023-10-27 11:55:12 -0700

    commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,24 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
  * is_positive_number - Check if a string is a positive integer.
  * @str: The input string to check.
  * Return: 1 if positive integer, 0 otherwise.
  */
-int is_positive_number(const char *str) {
-    if (str == NULL || *str == '\0') {
-        return 0;
-    }
+int is_positive_number(const char *str)
+{
+if (str == NULL || *str == '\0')
+return (0);
 
-    while (*str) {
-        if (*str < '0' || *str > '9') {
-            return 0;
-        }
-        str++;
-    }
+while (*str)
+{
+if (*str < '0' || *str > '9')
+return (0);
+str++;
+}
 
-    return 1;
+return (1);
 }
 /**
  * main - Check if a string is a positive integer.
@@ -26,25 +25,29 @@ int is_positive_number(const char *str) {
  * @argv: The input string to check.
  * Return: 1 if positive integer, 0 otherwise.
  */
-int main(int argc, char *argv[]) {
-    int sum = 0;
-    int i;
+int main(int argc, char *argv[])
+{
+int sum = 0;
+int i;
 
-    if (argc == 1) {
-        printf("0\n");
-        return 0;
-    }
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
 
-    for (i = 1; i < argc; i++) {
-        if (is_positive_number(argv[i])) {
-            sum += atoi(argv[i]);
-        } else {
-            printf("Error\n");
-            return 1;
-        }
-    }
+for (i = 1; i < argc; i++)
+{
+if (is_positive_number(argv[i]))
+sum += atoi(argv[i]);
+else
+{
+printf("Error\n");
+return (1);
+}
+}
 
-    printf("%d\n", sum);
-    return 0;
+printf("%d\n", sum);
+return (0);
 }
 
