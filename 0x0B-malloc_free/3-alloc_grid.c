48f797b7f7fc2b74b2fc4dbc630f29415396d010
osama <osamamahmoudac@gmail.com>
2023-10-30 13:10:00 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,12 +17,12 @@ return (NULL);
 
 array = (int**)malloc(height * sizeof(int*));
 if (array == NULL)
-return (1);
+return (NULL);
 for (i = 0; i < height; i++)
 {
 array[i] = (int*)malloc(width * sizeof(int));
 if (array[i] == NULL)
-return (1);
+return (NULL);
 
 for (j = 0; j < width; j++)
 array[i][j] = 0;
