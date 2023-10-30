d8937bd3526cb1abd95fe49d936fe828a8e60a4e
osama <osamamahmoudac@gmail.com>
2023-10-30 09:37:58 -0700

    commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,10 +16,11 @@ if (str == NULL)
 return (NULL);
 for (i = 0; str[i]; i++)
 i++;
-s = malloc(i+1 *sizeof(char));
+s = malloc(i + 1 * sizeof(char));
 if (s == NULL)
 return (NULL);
 for (i = 0; str[i]; i++)
 s[i] = str[i];
 return (s);
 }
+
