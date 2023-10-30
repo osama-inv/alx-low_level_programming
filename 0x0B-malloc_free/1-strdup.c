cd2cd878557d7c50aeb700073e7e9c171ece1837
osama <osamamahmoudac@gmail.com>
2023-10-30 08:19:55 -0700

    commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - a def that does something.
+ * @str: char.
+ * Return: Always 0 (Success)
+ */
+
+char *_strdup(char *str)
+{
+unsigned int i;
+char *s;
+
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i]; i++)
+i++;
+s = malloc(i * sizeof(char));
+if (s == NULL)
+return (NULL);
+for (i = 0; str[i]; i++)
+s[i] = str[i];
+return (s);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
 
 
 
