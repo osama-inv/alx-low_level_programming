8dea6641aacac6f54351f14572fe84468d3cb33e
osama <osamamahmoudac@gmail.com>
2023-10-30 14:35:02 -0700

    commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,79 +1,77 @@
 #include "main.h"
 #include <stdlib.h>
 
+
 /**
- * count_words - Counts the number of words in a string.
- * @str: The string to be counted.
+ * wrdcnt - counts the number of words in a string
+ * @s: string to count
  *
- * Return: The number of words in @str.
+ * Return: int of number of words
  */
-int count_words(char *str)
+int wrdcnt(char *s)
 {
-    int words = 0, i = 0;
+	int i, n = 0;
 
-    while (*(str + i))
-    {
-        while (*(str + i) == ' ')
-            i++;
-
-        if (*(str + i))
-            words++;
-
-        while (*(str + i) && *(str + i) != ' ')
-            i++;
-    }
-
-    return (words);
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+				n++;
+		}
+		else if (i == 0)
+			n++;
+	}
+	n++;
+	return (n);
 }
 
 /**
- * strtow - Splits a string into words.
- * @str: The string to be split.
+ * strtow - splits a string into words
+ * @str: string to split
  *
- * Return: If str is NULL, empty, or on failure - NULL.
- *         Otherwise - a pointer to an array of strings (words).
+ * Return: pointer to an array of strings
  */
 char **strtow(char *str)
 {
-    char **strings;
-    int i = 0, j, k, len, words;
+	int i, j, k, l, n = 0, wc = 0;
+	char **w;
 
-    if (str == NULL || *str == '\0')
-        return (NULL);
-
-    words = count_words(str);
-    if (words == 0)
-        return (NULL);
-
-    strings = malloc(sizeof(char *) * (words + 1));
-    if (strings == NULL)
-        return (NULL);
-
-    while (*str)
-    {
-        while (*str == ' ')
-            str++;
-
-        len = 0;
-        while (*(str + len) && *(str + len) != ' ')
-            len++;
-
-        *(strings + i) = malloc(sizeof(char) * (len + 1));
-        if (*(strings + i) == NULL)
-        {
-            for (j = 0; j < i; j++)
-                free(*(strings + j));
-            free(strings);
-            return (NULL);
-        }
-
-        for (k = 0; k < len; k++)
-            *(*(strings + i) + k) = *(str++);
-        *(*(strings + i) + k) = '\0';
-        i++;
-    }
-    *(strings + i) = NULL;
-
-    return (strings);
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	n = wrdcnt(str);
+	if (n == 1)
+		return (NULL);
+	w = (char **)malloc(n * sizeof(char *));
+	if (w == NULL)
+		return (NULL);
+	w[n - 1] = NULL;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+			j++;
+			w[wc] = (char *)malloc(j * sizeof(char));
+			j--;
+			if (w[wc] == NULL)
+			{
+				for (k = 0; k < wc; k++)
+					free(w[k]);
+				free(w[n - 1]);
+				free(w);
+				return (NULL);
+			}
+			for (l = 0; l < j; l++)
+				w[wc][l] = str[i + l];
+			w[wc][l] = '\0';
+			wc++;
+			i += j;
+		}
+		else
+			i++;
+	}
+	return (w);
 }
-
