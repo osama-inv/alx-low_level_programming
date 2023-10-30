6b553d75779fc431b33bf42d4ed5a62f5294e726
osama <osamamahmoudac@gmail.com>
2023-10-30 13:05:50 -0700

    commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - a def that does something.
+ * @width: char.
+ * @height: char.
+ * Return: Always 0 (Success)
+ */
+
+int **alloc_grid(int width, int height)
+{
+if (width <= 0 || height <= 0)
+return (NULL);
+
+int** array = (int**)malloc(height * sizeof(int*));
+if (array == NULL)
+return (1);
+for (int i = 0; i < height; i++)
+{
+array[i] = (int*)malloc(width * sizeof(int));
+if (array[i] == NULL)
+return (1);
+
+for (int j = 0; j < width; j++)
+array[i][j] = 0;
+}
+return (array);
+}
