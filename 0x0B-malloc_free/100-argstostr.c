43c8270d6ecb7dd6e71ac17055e058d4b00d03af
osama <osamamahmoudac@gmail.com>
2023-10-30 13:35:38 -0700

    commit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - a def that does something.
+ * @ac: char.
+ * @av: char.
+ * Return: Always 0 (Success)
+ */
+
+char *argstostr(int ac, char **av)
+{
+int i, j;
+int num = 0;
+char *get;
+for (i = 0; i < ac; i++)
+for (j = 0; av[i][j]; j++)
+num++;
+num += ac - 1;
+
+get = malloc(sizeof(char) * num);
+for (i = 0; i < ac; i++)
+for (j = 0; av[i][j]; j++)
+{
+get[i][j] = av[i][j];
+if (!av[i][j + 1])
+get[i][j] = "\n";
+}
+}
