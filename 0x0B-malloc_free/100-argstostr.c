053db2a1a1e3af605998e3cd6a414188136a5fd0
osama <osamamahmoudac@gmail.com>
2023-10-30 13:39:14 -0700

    commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,10 +13,16 @@ char *argstostr(int ac, char **av)
 int i, j;
 int num = 0;
 char *get;
+if (ac <= 0 || av == NULL)
+return (NULL);
+
 for (i = 0; i < ac; i++)
+{
 for (j = 0; av[i][j]; j++)
 num++;
-num += ac - 1;
+}
+
+num += ac + 1;
 
 get = malloc(sizeof(char) * num);
 for (i = 0; i < ac; i++)
