f3ab015589baca19ea1bbff1396ddadd5b28514c
osama <osamamahmoudac@gmail.com>
2023-10-30 13:08:13 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,19 +11,20 @@
 int **alloc_grid(int width, int height)
 {
 int** array;
+int i, j;
 if (width <= 0 || height <= 0)
 return (NULL);
 
 array = (int**)malloc(height * sizeof(int*));
 if (array == NULL)
 return (1);
-for (int i = 0; i < height; i++)
+for (i = 0; i < height; i++)
 {
 array[i] = (int*)malloc(width * sizeof(int));
 if (array[i] == NULL)
 return (1);
 
-for (int j = 0; j < width; j++)
+for (j = 0; j < width; j++)
 array[i][j] = 0;
 }
 return (array);
