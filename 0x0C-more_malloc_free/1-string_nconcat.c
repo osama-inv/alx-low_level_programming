5a866565fff0d7aae1f40deca2549ad6a24c4a9c
osama <osamamahmoudac@gmail.com>
2023-11-02 11:25:43 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,44 @@
 #include <stdlib.h>
+
 /**
- * string_nconcat - function that concatenates two strings
- *
- * @s1: string 1
- * @s2: string 2
- * @n: bytes of @s2 to add to @s1 to be a new string
- *
- * Return: new string followed by the first @n bytes
- *         of string 2 @s2 or NULL
-*/
+ * string_nconcat - concatenates two strings.
+ * @s1: char
+ * @s2: char
+ * @n: int
+ * Return: sus.
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1, l2, i, j;
-	char *str;
+    char *full;
+    int fr = 0, sum = 0, sc = 0, i, j;
 
-	/*treat NULL as empty string*/
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	l1 = l2 = 0;
-	/*length of both s1 & s2 respectively*/
-	while (s1[l1] != '\0')
-		l1++;
-	while (s2[l2] != '\0')
-		l2++;
-
-	/*set n to length of s2*/
-	if (n >= l2)
-		n = l2;
-
-	str = (char *) malloc((l1 + n + 1) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-
-	/*add s1 to str*/
-	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s1[i];
-	/*add s2 n bytes to str*/
-	for (j = 0; j < n && s2[j] != '\0'; j++)
-	{
-		str[i] = s2[j];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+    if (!s1 && !s2)
+        return (NULL);
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+    while (s1[fr] || s2[sc])
+    {
+        if (s1[fr])
+            fr++;
+        if (s2[sc])
+            sc++;
+    }
+    if (n > sc)
+        n = sc;
+    sum = fr + n;
+    full = malloc(sum + 1);
+    if (!full)
+        return(NULL);
+    for (i = 0; i < fr; i++)
+        full[i] = s1[i];
+    for (j = 0; j < fr; j++)
+    {
+        full[i] = s1[j];
+        j++;
+    }
+    full[i] = '\0';
+    return (full);
 }
