be4d3836e415937895dd4e9e009bd876a22375f1
osama <osamamahmoudac@gmail.com>
2023-11-02 11:36:37 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,50 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings.
- * @s1: char
- * @s2: char
- * @n: int
- * Return: sus.
- */
+ * string_nconcat - concatenates two strings
+ *
+ * @s1:  1
+ * @s2:  2
+ * @n: bytes of @s2 
+ *
+ * Return: new string
+*/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *full;
-unsigned int fr, sum, sc, i, j;
+	unsigned int l1, l2, i, j;
+	char *str;
 
-if (!s1 && !s2)
-return (NULL);
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-fr = sc = 0;
-while (s1[fr] != '\0')
-fr++;
-while (s2[sc] != '\0')
-sc++;
-if (n > sc)
-n = sc;
-sum = fr + n;
-full = malloc((sum + 1) * sizeof(char));
-if (!full)
-return (NULL);
-for (i = 0; i < fr; i++)
-full[i] = s1[i];
-for (j = 0; j < sc && s1[j] != '\0'; j++)
-{
-full[i] = s1[j];
-j++;
-}
-full[i] = '\0';
-return (full);
+	/*treat NULL as empty string*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	l1 = l2 = 0;
+	/*length of both s1 & s2 respectively*/
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
+
+	/*set n to length of s2*/
+	if (n >= l2)
+		n = l2;
+
+	str = (char *) malloc((l1 + n + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	/*add s1 to str*/
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	/*add s2 n bytes to str*/
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
