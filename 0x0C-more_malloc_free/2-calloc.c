1d69454a7341c718b9532a6619bb9f1f29de22cd
osama <osamamahmoudac@gmail.com>
2023-11-02 11:49:51 -0700

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,39 +1,51 @@
 #include <stdlib.h>
 
 /**
- * _memset - fills memory with 0
+ * _memset - fills memory with a constant byte
  *
- * @s: input pointer
- * @b: characters
- * @n: number of bytes
+ * @s: input pointer that represents memory
+ *     block to fill
+ * @b: characters to fill
+ * @n: number of bytes to be filled
  *
- * Return: A pointer to the array
+ * Return: A pointer to the filled memory area
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+	unsigned int i;
 
-for (i = 0; i < n; i++)
-s[i] = b;
-return (s);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
+
 /**
- * _calloc - fills memory with 0
+ * _calloc - a function that allocates
+ *           memory for an array using malloc
  *
- * @nmemb: input pointer
- * @size: characters
- * Return: A pointer to the array
+ *           It is basically the equivalent to
+ *           malloc followed by memset
+ *
+ * @nmemb: size of array
+ * @size: size of each element
+ *
+ * Return: pointer with new allocated memory
+ *         or NULL if it fails
 */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *arr;
+	char *p;
 
-if (size == 0 || nmemb == 0)
-return (NULL);
-arr = malloc(nmemb * size);
-if (arr == NULL)
-return (NULL);
-_memset(p, 0, nmemb * size);
-return (arr);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	_memset(p, 0, nmemb * size);
+
+	return (p);
 }
