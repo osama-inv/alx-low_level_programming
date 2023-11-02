94bdcd51435bc1be44f1f3b28a59a373bee5da98
osama <osamamahmoudac@gmail.com>
2023-11-02 11:27:08 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
     char *full;
-    int fr, sum, sc, i, j;
+    unsigned fr, sum, sc, i, j;
 
     if (!s1 && !s2)
         return (NULL);
