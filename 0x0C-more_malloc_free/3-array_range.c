38e2a88cb93243de1b533783a67428e58a81e58d
osama <osamamahmoudac@gmail.com>
2023-11-02 12:08:28 -0700

    commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,23 +1,6 @@
 #include <stdlib.h>
 
 /**
- * fill - fills memory with 0
- *
- * @min: input pointer
- * @max: characters
- * @arr: number of bytes
- *
- * Return: A pointer to the array
-*/
-
-void fill(int min, int max, int *arr)
-{
-int i, tag = 0;
-for (i = min; i <= max; i++)
-arr[tag++] = i;
-}
-
-/**
  * array_range - fills memory with 0
  *
  * @min: input pointer
@@ -28,13 +11,14 @@ arr[tag++] = i;
 
 int *array_range(int min, int max)
 {
-int *arr;
-int elms;
+int i, tag, *arr;
 
 if (min > max)
 return (NULL);
-elms = 1 + (max - min);
-arr = malloc(elms * sizeof(int));
-fill(min, max, arr);
+arr = malloc((1 + (max - min)) * sizeof(int));
+if (arr == NULL)
+return (NULL);
+for (i = min; i <= max; i++)
+arr[tag++] = i;
 return (arr);
 }
