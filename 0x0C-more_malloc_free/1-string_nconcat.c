25c72adbc8c8eb74ec4ad2c367dd3dd69eda6854
osama <osamamahmoudac@gmail.com>
2023-11-02 10:55:03 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,35 +13,39 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *full;
-    int fr = 0, sum = 0, sc = 0, i, closed = 1;
+char *full;
+int fr = 0, sum = 0, sc = 0, i, closed = 1;
 
-    if (!s1 && !s2)
-        return (NULL);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-    while (s1[fr] || s2[sc])
-    {
-        if (s1[fr])
-            fr++;
-        if (s2[sc])
-            sc++;
-    }
-    if (n < sc)
-        sc = n;
-    sum = fr + sc + 1;
-    full = malloc(sum);
-    if (!full || sum == 0)
-        return(NULL);
-    for (i = 0; i < sum - 1; i++)
-    {
-        if (s1[i] && closed)
-        full[i] = s1[i];
-        else
-        {
-            closed = 0;
-            full[i] = s2[i - fr];
-        }
-    }
-    full[i] = '\0';
-    return (full);
+if (!s1 && !s2)
+return (NULL);
+while (s1[fr] || s2[sc])
+{
+if (s1[fr])
+fr++;
+if (s2[sc])
+sc++;
+}
+if (n < sc)
+sc = n;
+sum = fr + sc + 1;
+full = malloc(sum);
+if (!full || sum == 0)
+return(NULL);
+for (i = 0; i < sum - 1; i++)
+{
+if (s1[i] && closed)
+full[i] = s1[i];
+else
+{
+closed = 0;
+full[i] = s2[i - fr];
+}
+}
+full[i] = '\0';
+return (full);
 }
