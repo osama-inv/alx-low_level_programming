ecaeec36740b0afbee702c660f18251f9f551cfb
osama <osamamahmoudac@gmail.com>
2023-11-02 11:04:39 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,46 @@
+#include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	char *p = s;
-
-	while (*s)
-		s++;
-	return (s - p);
-}
-
-/**
- * string_nconcat - counts the number of words in a string
- * @s1: string to count
- * @s2: string to count
- * @n: string to count
- *
- * Return: int of number of words
+ * string_nconcat - concatenates two strings.
+ * @s1: char
+ * @s2: char
+ * @n: int
+ * Return: sus.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, size, len1, len2;
 	char *ptr;
+	unsigned int i, len1, len2;
 
-	if (s1 == NULL)
+	if (!s1)
 		s1 = "";
-	if (s2 == NULL)
+	if (!s2)
 		s2 = "";
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	if (n > len2)
+
+	for (len1 = 0; *(s1 + len1); len1++)
+		;
+	for (len2 = 0; *(s2 + len2); len2++)
+		;
+
+	if (len2 < n)
 		n = len2;
-	size = len1 + n;
-	ptr = malloc(size + 1);
+
+	ptr = malloc((len1 + n + 1) * sizeof(char));
+
 	if (!ptr)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-		ptr[i] = s1[i];
-	for (j = 0; j < n; j++, i++)
-		ptr[i] = s2[j];
+
+	for (i = 0; i < (len1 + n); i++)
+	{
+		if (i < len1)
+			ptr[i] = s1[i];
+		else
+			ptr[i] = s2[i - len1];
+	}
 	ptr[i] = '\0';
+
 	return (ptr);
 }
