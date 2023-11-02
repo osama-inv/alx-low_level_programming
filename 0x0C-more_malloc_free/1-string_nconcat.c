03c6e8a74724b8d7843782cdf7a6a49a68068bc9
osama <osamamahmoudac@gmail.com>
2023-11-02 11:37:57 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings
+ * string_nconcat - function that concatenates two strings
  *
- * @s1:  1
- * @s2:  2
- * @n: bytes of @s2 
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes of @s2 to add to @s1 to be a new string
  *
- * Return: new string
+ * Return: new string followed by the first @n bytes
+ *         of string 2 @s2 or NULL
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
