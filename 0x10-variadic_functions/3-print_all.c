50cbcc3a726401cbfc4f064fcd684a369b15d9f6
osama <osamamahmoudac@gmail.com>
2023-11-09 05:40:22 -0800

    commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,46 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
  * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * @format: format of data type to be printed
  */
 void print_all(const char * const format, ...)
 {
-    va_list args; /* declare a va_list object */
-    char c; /* declare a char variable */
-    int i = 0; /* declare an int variable */
+	unsigned int i = 0;
+	char *str, *sep = "";
+	va_list arguments;
 
-    va_start(args, format); /* initialize the va_list object with the first fixed argument */
-    while (format[i] != '\0') /* loop over the format string */
-    {
-        c = format[i]; /* assign the current character to the char variable */
-        i++; /* increment the index variable */
-        if (c == 'c' || c == 'i' || c == 'f' || c == 's') /* check if the current character is a valid type */
-        {
-            switch (c) /* handle each type separately */
-            {
-                case 'c': /* if the type is char */
-                    printf("%c", va_arg(args, int)); /* get the next argument as an int and print it as a char */
-                    break; /* exit the switch statement */
-                case 'i': /* if the type is int */
-                    printf("%d", va_arg(args, int)); /* get the next argument as an int and print it as an int */
-                    break; /* exit the switch statement */
-                case 'f': /* if the type is float */
-                    printf("%f", va_arg(args, double)); /* get the next argument as a double and print it as a float */
-                    break; /* exit the switch statement */
-                case 's': /* if the type is string */
-                    char *s = va_arg(args, char *); /* get the next argument as a char * and assign it to a char * variable */
-                    if (s == NULL) /* check if the string is NULL */
-                        s = "(nil)"; /* assign "(nil)" to the char * variable */
-                    printf("%s", s); /* print the char * variable as a string */
-                    break; /* exit the switch statement */
-            }
-        }
-        else /* handle any other character */
-        {
-            continue; /* ignore the character and continue the loop */
-        }
-    }
-    va_end(args); /* clean up the va_list object */
-    printf("\n"); /* print a new line */
+	va_start(arguments, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(arguments, int));
+					break;
+				case 'i':
+					printf("%s%i", sep, va_arg(arguments, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(arguments, double));
+					break;
+				case 's':
+					str = va_arg(arguments, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(arguments);
 }
+
