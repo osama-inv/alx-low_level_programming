0ccf4f546cf940c627ad96c289f5581525e82cd4
osama <osamamahmoudac@gmail.com>
2023-11-09 02:47:55 -0800

    commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - def
+ * @n: argc.
+ * Return: sum.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+int sum = 0, i;
+va_list args;
+va_start(args, n);
+if (n == 0)
+return (0);
+for (i = 0; i < n; ++i)
+sum += va_arg(args, unsigned int);
+va_end(args);
+return (sum);
+}
