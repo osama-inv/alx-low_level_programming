08e2d65c77dbe44440a159b96d22860cb7a688d9
osama <osamamahmoudac@gmail.com>
2023-11-09 02:49:23 -0800

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,11 +9,12 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-int sum = 0, i;
+int sum, i;
 va_list args;
 va_start(args, n);
 if (n == 0)
 return (0);
+sum = 0;
 for (i = 0; i < n; ++i)
 sum += va_arg(args, unsigned int);
 va_end(args);
