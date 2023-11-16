7fa486be4ea274cac3424b8d388026320a3a6797
osama-inv <osamamahmoudac@gmail.com>
2023-11-16 08:08:49 -0800

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * print_before_main - *
+ * Return: void
+ */
 void __attribute__((constructor)) print_before_main()
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
 }
+
