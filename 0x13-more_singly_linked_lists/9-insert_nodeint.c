9531e23f9b0b53bf80ef456e5d7d1c2805bdeced
osama-inv <osamamahmoudac@gmail.com>
2023-11-21 10:58:34 -0800

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,35 +8,29 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i = 0;
+unsigned int i;
 listint_t *h = *head;
-listint_t *after;
 listint_t *new_node;
+
+for (i = 0; h && i < idx - 1; i++)
+h = h->next;
+
 if (h == NULL && idx != 0)
 return (NULL);
+
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+
+new_node->n = n;
+
 if (idx == 0)
 {
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = n;
 new_node->next = *head;
 *head = new_node;
-}
-for ( ;h && i < idx; i++)
-{
-if ((i + 1) == idx)
-{
-after = h->next;
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = n;
-new_node->next = after;
-h->next = new_node;
 return (new_node);
 }
-h = h->next;
-}
-return (NULL);
+new_node->next = h->next;
+h->next = new_node;
+return (new_node);
 }
