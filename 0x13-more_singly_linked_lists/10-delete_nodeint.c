afc9653c16d3d48c531fb6bcd47ace699477a423
osama-inv <osamamahmoudac@gmail.com>
2023-11-21 11:15:37 -0800

    commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,27 +7,35 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *h = *head;
-listint_t *delet;
-unsigned int i;
-
-for (i = 0; h && i < index - 1; i++)
-h = h->next;
-
-if (h == NULL && index != 0)
+listint_t *hold, *delete;
+unsigned int i, action = 0;
+if (!*head)
 return (-1);
-
-if (index == 0)
+for (i = 0, hold = *head; hold && index; i++, hold = (*hold).next)
+if (i == (index - 1))
 {
-delet = *head;
-*head = (*head)->next;
+action = 1;
+break;
 }
-else
+if (action)
 {
-delet = h->next;
-if (delet)
-h->next = delet->next;
-}
-free(delet);
+delete = (*hold).next;
+(*hold).next = (*delete).next;
+free(delete);
 return (1);
+}
+else if (!index && (**head).next)
+{
+delete = *head;
+*head = (*delete).next;
+free(delete);
+return (1);
+}
+else if (!index && *head)
+{
+*head = NULL;
+free(*head);
+return (1);
+}
+return (-1);
 }
