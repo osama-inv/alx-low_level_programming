eb2f68de0bcd2cf1ab560d7f5d7b5f916321ab8b
osama-inv <osamamahmoudac@gmail.com>
2023-11-21 04:20:30 -0800

    commit

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,11 +7,11 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *neww = malloc(sizeof(listint_t)); /* Allocate memory for the new node*/
+listint_t *neww = malloc(sizeof(listint_t));
 if (neww == NULL)
 return (NULL); /* Check if allocation failed*/
 neww->n = n; /* Set the string value of the new node*/
-neww->next = *head; /* Set the next of the new node to the current head*/
+neww->next = *head; /* Set the next of the new node*/
 *head = neww;
 return (neww); /* Return the new node*/
 }
