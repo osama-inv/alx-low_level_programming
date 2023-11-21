beed6d8f5890e54c4e26f65c734c304e9622b258
osama-inv <osamamahmoudac@gmail.com>
2023-11-21 04:19:19 -0800

    commit

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * add_nodeint - singly linked list
+ * @n: --
+ * @head: --
+ * Return: new address
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *neww = malloc(sizeof(listint_t)); /* Allocate memory for the new node*/
+if (neww == NULL)
+return (NULL); /* Check if allocation failed*/
+neww->n = n; /* Set the string value of the new node*/
+neww->next = *head; /* Set the next of the new node to the current head*/
+*head = neww;
+return (neww); /* Return the new node*/
+}
