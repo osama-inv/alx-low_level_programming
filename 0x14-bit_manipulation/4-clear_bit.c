899e81f6b3b6ecf3fcd3b1c7b564125a452e6095
osama-inv <osamamahmoudac@gmail.com>
2023-11-23 13:52:35 -0800

    commit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,13 +2,13 @@
 /**
  * clear_bit - sets the value of a bit to 0 at a given index
  * @n: pointer to the number
- * @index: index starting from 0 of the bit to clear
+ * @index: index starting from 0 of the bit to set
  * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 if (index >= (sizeof(unsigned long int) * 8))
 return (-1);
-*n ^= (1 << index);
+*n &= ~(1 << index);
 return (1);
 }
