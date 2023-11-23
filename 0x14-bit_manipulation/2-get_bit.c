4d7998f99d209fc0bf5ffe7afa0ff2713ad1f71c
osama-inv <osamamahmoudac@gmail.com>
2023-11-23 08:06:42 -0800

    commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to be checked
+ * @index: index starting from 0 of the bit to get
+ * Return: value of the bit, or -1 if an error occurred
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+    if (index > (sizeof(n) * 8))
+        return (-1);
+
+    return ((n >> index) & 1);
+}
+
