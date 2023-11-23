7d6543d120b39af941f817da470d8541ca1a3f9e
osama-inv <osamamahmoudac@gmail.com>
2023-11-23 14:31:55 -0800

    commit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * get_endianness - returns the number of bits you would need to flip
+ * Return: number of bits you would need to flip
+ */
+int get_endianness(void)
+{
+unsigned int x = 0x76543210;
+char *c = (char*) &x;
+return (*c == 0x10);
+}
