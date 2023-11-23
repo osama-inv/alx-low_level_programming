91044e22a444b69778635f21388bb5a32589603e
osama-inv <osamamahmoudac@gmail.com>
2023-11-23 05:33:32 -0800

    commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert
+ * @b: --
+ * Return: num
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int num = 0;
+int power = 1, len = 0;
+while (b[len])
+{
+if (b[len] == '0' || b[len] == '1')
+len++;
+else
+return (0);
+}
+len--;
+while (len >= 0)
+{
+if (b[len] & 1)
+num += power;
+power *= 2;
+len--;
+}
+return (num);
+}
