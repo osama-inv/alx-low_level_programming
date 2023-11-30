5afb6d32de8ccd3b269001e07edc9789c3194e5b
osama-inv <osamamahmoudac@gmail.com>
2023-11-30 05:58:20 -0800

    commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * append_text_to_file - add text.
+ * @filename: filename.
+ * @text_content: added content.
+ * Return: 1 or -1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int num;
+int ret;
+int file;
+if (!filename)
+return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (file == -1)
+return (-1);
+if (text_content)
+{
+for (num = 0; text_content[num]; num++)
+;
+tre = write(file, text_content, num);
+if (ret == -1)
+return (-1);
+}
+close(file);
+return (1);
+}
