7a82c48a31a40d268a38a8cdf90a6b77b99da022
osama-inv <osamamahmoudac@gmail.com>
2023-11-30 05:40:43 -0800

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,40 +6,38 @@
   * @letters: num of letters
   * Return: --
   */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *place;
-	size_t gett, see;
-	int filen;
-	if (!filename || !letters)
-	{
-		return (0);
-	}
-	place = malloc(letters);
-	if (!place)
-    {
-        return (0);
-    }
-	/* get it open */
-	filen = open(filename, O_RDONLY);
-	if (filen == -1)
-	{
-		free(place);
-		return (0);
-	}
-	gett = read(filen, place, letters);
-	if (gett < 1)
-	{
-		free(place);
-		close(filen);
-		return (0);
-	}
-	see = write(STDOUT_FILENO, place, gett);
-	free(place);
-	close(filen);
-
-	if (!see || see != gett)
-		return (0);
-	return (gett);
+char *place;
+size_t gett, see;
+int filen;
+if (!filename || !letters)
+{
+return (0);
+}
+place = malloc(letters);
+if (!place)
+{
+return (0);
+}
+/* get it open */
+filen = open(filename, O_RDONLY);
+if (filen == -1)
+{
+free(place);
+return (0);
+}
+gett = read(filen, place, letters);
+if (gett < 1)
+{
+free(place);
+close(filen);
+return (0);
+}
+see = write(STDOUT_FILENO, place, gett);
+free(place);
+close(filen);
+if (!see || see != gett)
+return (0);
+return (gett);
 }
