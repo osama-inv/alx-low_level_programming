aae3ac19e64e5897829d44827c664756f07a7926
osama-inv <osamamahmoudac@gmail.com>
2023-11-30 05:44:20 -0800

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,23 +7,22 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
-	ssize_t num, nwr;
-	char *buf;
-
-	if (!filename)
-    {
-        return (0);
-    }
-	file = open(filename, O_RDONLY);
-	if (file == -1)
-		return (0);
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
-	num = read(file, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, num);
-	close(file);
-	free(buf);
-	return (nwr);
+int file;
+ssize_t num, nwr;
+char *buf;
+if (!filename)
+{
+return (0);
+}
+file = open(filename, O_RDONLY);
+if (file == -1)
+return (0);
+buf = malloc(sizeof(char) * (letters));
+if (!buf)
+return (0);
+num = read(file, buf, letters);
+nwr = write(STDOUT_FILENO, buf, num);
+close(file);
+free(buf);
+return (nwr);
 }
