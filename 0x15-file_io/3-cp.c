48afccacd1d31e306e94cabcb158a5c56932047b
osama-inv <osamamahmoudac@gmail.com>
2023-11-30 06:28:24 -0800

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * error_file - checks if files can be opened.
- * @getHere: getHere.
- * @GiveTo: GiveTo.
+ * @file_from: file_from.
+ * @file_to: file_to.
  * @argv: arguments vector.
  * Return: no return.
  */
-void error_file(int getHere, int GiveTo, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
-	if (getHere == -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (GiveTo == -1)
+	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -29,37 +30,42 @@ void error_file(int getHere, int GiveTo, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int getHere, GiveTo, ErrorMis;
+	int file_from, file_to, err_close;
 	ssize_t nchars, nwr;
 	char buf[1024];
+
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp getHere GiveTo");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-	getHere = open(argv[1], O_RDONLY);
-	GiveTo = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(getHere, GiveTo, argv);
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
 	nchars = 1024;
 	while (nchars == 1024)
 	{
-		nchars = read(getHere, buf, 1024);
+		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(GiveTo, buf, nchars);
+		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
-	ErrorMis = close(getHere);
-	if (ErrorMis == -1)
+
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", getHere);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	ErrorMis = close(GiveTo);
-	if (ErrorMis == -1)
+
+	err_close = close(file_to);
+	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", getHere);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
