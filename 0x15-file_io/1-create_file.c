1a0141b7847069ca3894a305ae24d9746c031ba8
osama-inv <osamamahmoudac@gmail.com>
2023-11-30 05:53:18 -0800

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,22 +7,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int numm;
-int ret;
-int file;
+int nletters;
+int rwr;
+int fd;
 if (!filename)
 {
 return (-1);
 }
-file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (file == -1)
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
 return (-1);
 if (!text_content)
 text_content = "";
-for (numm = 0; text_content[numm]; numm++);
-ret = write(file, text_content, numm);
-if (ret == -1)
+for (nletters = 0; text_content[nletters]; nletters++)
+;
+rwr = write(fd, text_content, nletters);
+if (rwr == -1)
 return (-1);
-close(file);
+close(fd);
 return (1);
 }
