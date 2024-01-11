78f06467a5450b77ba5e1f52a00caa2345c9880b
osama-inv <osamamahmoudac@gmail.com>
2024-01-11 03:32:55 -0800

    commit

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the nodes
+ * @head: the head
+ * Return: sum of the all nodes
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+
+	return (sum);
+}
